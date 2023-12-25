ffd4a64e9704dc5a0f3c2007b91157d74ca23bc3
ted <theo_drz@live.com>
2023-12-24 22:26:15 -0500

    still trying to figure out part 2 for day 23

--- solutions/day23/p2v2.ts
+++ solutions/day23/p2v2.ts
@@ -9,7 +9,9 @@ interface QueueItem {
   row: number,
   col: number,
   stepCount: number,
-  seen: string[],
+  // seen: string[],
+  // intersections: number[][],
+  intersections: QueueItem[],
 }
 
 function isSurrounded(map: string[][], row: number, col: number) {
@@ -23,46 +25,81 @@ function isSurrounded(map: string[][], row: number, col: number) {
   }).filter(isHash => isHash).length
 }
 
-// function walkInOrder(map: string[], nextSteps: number[][], end: number[], graph: any, seen: string[]) {
-//   for (let i = 0; i < nextSteps.length; i++) {
-//     const { row, col, stepCount, seen } = nextSteps[i];
-//     if (row === end[0] && col === end[1]) {
-//       if (stepCount > best) {
-//         console.log('NEW BEST: ', stepCount)
-//         best = stepCount
-//       }
-//     }
-//     const newSeen = seen.concat([JSON.stringify([row, col])])
-// 
-//     // const curChar = map[row][col];
-//     // const test = 'v^<>'.includes(curChar) ? [dirs[curChar]] : Object.values(dirs);
-//     const test = Object.values(dirs)
-// 
-//     test.forEach(dir => {
-//       const [dy, dx] = dir;
-//       const [nextY, nextX] = [row + dy, col + dx];
-//       if (0 > nextY || nextY >= map.length) return
-//       if (0 > nextX || nextX >= map[0].length) return
-//       if (map[nextY][nextX] !== '#') {
-//         // const hashCount = isSurrounded(map, nextY, nextX)
-//         if (newSeen.includes(JSON.stringify([nextY, nextX]))) {
-//           return
-//         }
-//         newSteps.push({
-//           row: nextY,
-//           col: nextX,
-//           stepCount: stepCount + 1,
-//           seen: newSeen,
-//         });
-//       }
-//     })
-//   }
-// }
+function walkOnce(
+  map: string[][],
+  // paths: number[][][],
+  paths: QueueItem[],
+  // seen: string[],
+  end: number[],
+  answers: QueueItem[] = [],
+  seen: string[] = [],
+  // intersections: number[][],
+) {
+  while (paths.length > 0) {
+    // console.log('WHILE LOOP')
+    // console.log(paths)
+    const newPaths: QueueItem[] = []
+    // console.log('CHECKING PATHS: ', paths.length)
+    for (let i = 0; i < paths.length; i++) {
+      // const [row, col, stepCount, path] = paths[i][paths[i].length - 1];
+      const { row, col, stepCount, /*seen,*/ intersections } = paths[i];
+      if (row === end[0] && col === end[1]) {
+        console.log('END POINT')
+        answers.push(paths[i])
+        continue
+      }
+      // const surroundCount = isSurrounded(map, row, col)
+      // if (surroundCount < 2) {
+      //   intersections.push([row, col])
+      // }
+      let counter = 0;
+      Object.values(dirs).forEach(dir => {
+        const [dy, dx] = dir;
+        const [newY, newX] = [row + dy, col + dx];
+        if (0 > newY || newY >= map.length) return
+        if (0 > newX || newX >= map[0].length) return
 
-// We want to crawl the map one time
-// This should return some structure that indicates the relationships between intersections
-// i.e which intersections can point to which other instersection and the stepCount between them
-// Then just find the longest possible combination that involves none of the same intersection points
+        const newSurroundCount = isSurrounded(map, newY, newX)
+        let split = false;
+        if (newSurroundCount < 2) {
+          // console.log('NEXT POS IS INTERSECTION')
+          // console.log([newY, newX])
+          // console.log(paths[i])
+          split = true;
+          if (seen.includes(JSON.stringify([newY, newX]))) {
+            console.log('NEXT POS IS LOOP')
+            // intersections.push([newY, newX])
+            console.log(intersections)
+          }
+        }
+
+
+        if (map[newY][newX] !== '#' && !seen.includes(JSON.stringify([newY, newX]))) {
+          seen.push(JSON.stringify([newY, newX]))
+          // console.log('new move')
+          let intCopy = JSON.parse(JSON.stringify(intersections))
+            .concat({
+            row: newY,
+            col: newX,
+            stepCount: stepCount + 1
+          })
+          counter++
+          newPaths.push({
+            row: newY,
+            col: newX,
+            stepCount: stepCount + 1,
+            intersections: split ? intCopy : intersections,
+          })
+        }
+      });
+      if (counter === 0) {
+        console.log('DEAD END, no moves to make')
+      }
+    }
+    paths = newPaths;
+  }
+  return answers;
+}
 
 const map = (await Bun.file('example.txt').text())
 // const map = (await Bun.file('inputs.txt').text())
@@ -82,6 +119,15 @@ map.forEach((row, y) => {
   })
 })
 intersections.forEach(arr => console.log(arr))
+const test = walkOnce(map, [{
+  row: start[0],
+  col: start[1],
+  // seen: [],
+  stepCount: 0,
+  intersections: [],
+}], end)
+console.log(test)
+
 // const answer = longWalkV2(map, [{
 //   row: start[0],
 //   col: start[1],
