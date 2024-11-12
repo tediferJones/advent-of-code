a98bcf693f5c8d3b083f7a5bbe3d8084b8854709
ted <theo_drz@live.com>
2024-11-11 23:37:32 -0500

    visualized graph, starting to see the pattern, need to track cycles and find LCM between all clusters

--- solutions/day20/problem2.ts
+++ solutions/day20/problem2.ts
@@ -35,6 +35,14 @@ class Conjunction {
     this.sendersAll = [];
   }
 
+  allHigh() {
+    return Object.keys(this.senders).every(name => this.senders[name] === 'hi');
+  }
+
+  allLow() {
+    return Object.keys(this.senders).every(name => this.senders[name] === 'lo');
+  }
+
   receivePulse(modules: AllModules, type: Pulse, sender: string) {
     this.senders[sender] = type;
     const allHigh = Object.keys(this.senders).every(name => this.senders[name] === 'hi');
@@ -94,6 +102,27 @@ class AllModules {
   pushBtn(pushCount = Infinity) {
     let count = 0
     while (count < pushCount) {
+      // const state = Object.keys(this.modules).toSorted().map(key => {
+      //   const mod = this.modules[key]
+      //   if (mod.constructor.name === 'Conjunction') {
+      //     return (mod as Conjunction).allHigh();
+      //   } else if (mod.constructor.name === 'FlipFlop') {
+      //     return (mod as FlipFlop).isOn;
+      //   }
+      //   return null
+      // })
+      // const strState = JSON.stringify(state)
+      // if (history[strState]) {
+      //   console.log(history)
+      //   throw Error('CYCLE DETECTED AT:' + count)
+      // } else {
+      //   history[strState] = count
+      // }
+
+      // if ((this.modules['cs'] as Conjunction).allLow()) {
+      //   console.log('CS ALL LOW AT', count)
+      //   diffChecker.push(count)
+      // }
       this.pulseQueue = [ { from: 'button', type: 'lo', to: 'broadcaster' } ]
       while (this.pulseQueue.length) {
         const item = this.pulseQueue.shift();
@@ -105,7 +134,45 @@ class AllModules {
       }
       count++
       if (count % 100000 === 0) console.log(count.toLocaleString())
+      // console.log(this.modules['dr'])
+      // console.log('FINAL', this.modules['xr'])
+      // console.log('LEVEL 1', this.modules['dr'])
+      // console.log('LEVEL 2', [ 'qb', 'mp', 'ng', 'qt' ].map(nodeName => this.modules[nodeName]))
+      // console.log('LEVEL 3', [ 'cs', 'dx', 'ck', 'jh' ].map(nodeName => this.modules[nodeName]))
+
       // console.log('DONE WITH BUTTON PUSH:', count)
+      // console.log('LEVEL 2', [ 'qb', 'mp', 'ng', 'qt' ].map(nodeName => `${this.modules[nodeName].name}, senders: ${this.modules[nodeName].senders}`))
+
+      // console.log(
+      //   // @ts-ignore
+      //   Object.keys(this.modules['cs'].senders).map(key => this.modules['cs'].senders[key]),
+      //   count.toLocaleString(),
+      //   'CS',
+      //   (this.modules['cs'] as Conjunction).allLow(),
+      // )
+      // printConjState('cs', count)
+      // printConjState('dx', count)
+
+      // if ((this.modules['cs'] as Conjunction).allHigh()) {
+      if ((this.modules['dx'] as Conjunction).allHigh()) {
+        return console.log('CS ALL HIGH AT', count)
+        // Theory: cs will be all true at (8 * 16 * 32 * 64 * 128 * 256 * 512 * 1024) + 4
+      }
+
+      // if (printNext) {
+      //   console.log(
+      //     // @ts-ignore
+      //     Object.keys(this.modules['cs'].senders).map(key => this.modules['cs'].senders[key]),
+      //     count.toLocaleString(),
+      //     'CS',
+      //     (this.modules['cs'] as Conjunction).allLow(),
+      //   )
+      //   printNext = false
+      // }
+      // if ((this.modules['cs'] as Conjunction).allLow()) {
+      //   console.log('CS ALL LOW AT', count)
+      //   printNext = true
+      // }
     }
   }
 
@@ -137,10 +204,25 @@ class AllModules {
 // It would probably be faster to process the tree in reverse, starting from rx with a lo pulse, and tracing until we reach a button call
 
 const startTime = Bun.nanoseconds();
-const fileContent = await Bun.file('example.txt').text();
+// const fileContent = await Bun.file('example.txt').text();
 // const fileContent = await Bun.file('example2.txt').text();
-// const fileContent = await Bun.file('inputs.txt').text();
+const fileContent = await Bun.file('inputs.txt').text();
 const allMods = new AllModules();
+
+// { stateString: btnCount }
+const history: { [key: string]: number } = {}
+let printNext = false;
+const diffChecker: number[] = []
+
+function printConjState(modName: string, count: number) {
+  console.log(
+    // @ts-ignore
+    Object.keys(allMods.modules[modName].senders).map(key => allMods.modules[modName].senders[key]),
+    count.toLocaleString(),
+    modName,
+    (allMods.modules[modName] as Conjunction).allLow(),
+  )
+}
 
 fileContent
   .split(/\n/)
@@ -157,16 +239,50 @@ fileContent
     }
   });
 
-console.log(allMods)
-allMods.linkAll();
-// allMods.linkConjunctions()
-// allMods.pushBtn(1);
-const answer= allMods.pushBtn(1);
-console.log(allMods)
-console.log('trace from:', allMods.traceFrom('c', 'lo'))
+// const visData = fileContent
+//   .split(/\n/)
+//   .filter(line => line)
+//   .map(line => {
+//     // const [ match, type, name, recipientStr ] = line.match(/([%&]?)(\w+|broadcaster) -> (.+)/) || []
+//     // const recipients = recipientStr.split(', ')
+//     // if (type === '%') {
+//     //   allMods.modules[name] = new FlipFlop(recipients, name);
+//     // } else if (type === '&') {
+//     //   allMods.modules[name] = new Conjunction(recipients, name);
+//     // } else if (name === 'broadcaster') {
+//     //   allMods.modules[name] = new Broadcaster(recipients, name);
+//     // }
+// 
+//     const [ match, type, name, recipientStr ] = line.match(/([%&]?)(\w+|broadcaster) -> (.+)/) || []
+//     const recipients = recipientStr.split(', ')
+//     return recipients.map(recipient => `${name} ${recipient}`)
+//   });
+// console.log(visData.flat().join('\n'))
+
+// rx is only connected to dr
+// dr is a conjunction module so it must remember hi pulses for all its senders
+// dr is a recipient for qb, mp, ng and qt
+// all of the above recipients are also conjunction modules, so each of them must also remember hi pulses for all potentional senders
+
+// rx wants lo
+// dr wants all hi
+// qb, mp, ng, qt want all hi
+//
+
+// console.log(allMods)
+// allMods.linkAll();
+allMods.linkConjunctions()
+allMods.pushBtn();
+// diffChecker.reduce((_, count, i) => {
+//   console.log(count - diffChecker[i - 1])
+//   return 0
+// })
+// const answer= allMods.pushBtn(1);
+// console.log(allMods)
+// console.log('trace from:', allMods.traceFrom('c', 'lo'))
 // const answer = allMods.count.hi * allMods.count.lo;
 console.log(`TIME: ${(Bun.nanoseconds() - startTime) / 10**9}`)
-console.log(`ANSWER: ${answer}`)
+// console.log(`ANSWER: ${answer}`)
 // console.log(answer === 919383692)
 
 // ANSWER PART 1: 919383692
