0d363d386a34ced3140f383740bf9d5e9a0cd22e
ted <theo_drz@live.com>
2025-01-29 20:43:37 -0500

    completed day 24 part 2 and cleaned up code

--- 2019/day24/problem2.ts
+++ 2019/day24/problem2.ts
@@ -1,4 +1,5 @@
 type Position = { row: number, col: number }
+type RecMap = Record<number, string[][]>
 
 const directions = [
   { row:  0, col:  1 },
@@ -54,208 +55,31 @@ function createNewLevel() {
   return Array(5).fill(0).map(() => Array(5).fill('.'))
 }
 
-function passTimeRec(recMap: RecMap, maxTime: number, time = 0) {
-  // inner grids have positive index
-  // outer grids have negative index
-  //
-  // we actually have to modify recMap
-
-  console.log('TIME', time)
-  Object.keys(recMap).toSorted((a, b) => Number(a) - Number(b)).forEach(levelStr => {
-    console.log(levelStr)
-    printMap(recMap[Number(levelStr)])
-  })
-  const newRecMap = JSON.parse(JSON.stringify(recMap))
-  if (time === maxTime) return recMap
-  Object.keys(recMap).forEach(levelStr => {
-    const level = Number(levelStr)
-    // const map = recMap[level]
-    recMap[level].map((row, i) => {
-      return row.map((cell, j) => {
-        const pos = { row: i, col: j }
-        if (pos.row === 2 && pos.col === 2) return cell
-
-        // should probably be reduce,
-        // we can return more elements than dirs if dir leads to different level
-        const tempAdjBugCount = directions.reduce((cells, dir) => {
-          const adjPos = translatePos(pos, dir)
-          if (adjPos.row === 2 && adjPos.col === 2) {
-            // use level + 1
-            if (!recMap[level + 1]) {
-              recMap[level + 1] = createNewLevel()
-              newRecMap[level + 1] = createNewLevel()
-            }
-            if (pos.row === 1) {
-              if (pos.col !== 2) throw Error('wrong position')
-              return cells.concat(recMap[level + 1][0].map(cell => cell === '#'))
-            }
-            if (pos.row === 3) {
-              if (pos.col !== 2) throw Error('wrong position')
-              return cells.concat(recMap[level + 1][4].map(cell => cell === '#'))
-            }
-            if (pos.col === 1) {
-              if (pos.row !== 2) throw Error('wrong position')
-              const col = recMap[level + 1].map(row => row[0])
-              return cells.concat(col.map(cell => cell === '#'))
-            }
-            if (pos.col === 3) {
-              if (pos.row !== 2) throw Error('wrong position')
-              const col = recMap[level + 1].map(row => row[4])
-              return cells.concat(col.map(cell => cell === '#'))
-            }
-            throw Error('error handling inner grid')
-          }
-          // const adjChar = charAtPos(map, adjPos)
-          const adjChar = charAtPos(recMap[level], adjPos)
-          if (!adjChar) {
-            if ((adjPos.row === 5) || (adjPos.row === -1)) {
-              if ((adjPos.col === 5) || (adjPos.col === -1)) {
-                throw Error('outer grid corner case')
-              }
-            }
-            console.log('ADJ POS', adjPos)
-            // use level - 1
-            if (!recMap[level - 1]) {
-              recMap[level - 1] = createNewLevel()
-              newRecMap[level - 1] = createNewLevel()
-            }
-            if (adjPos.row === -1) {
-              return cells.concat(recMap[level - 1][1][2] === '#')
-            }
-            if (adjPos.row === 5) {
-              return cells.concat(recMap[level - 1][3][2] === '#')
-            }
-            if (adjPos.col === -1) {
-              return cells.concat(recMap[level - 1][2][1] === '#')
-            }
-            if (adjPos.col === 5) {
-              return cells.concat(recMap[level - 1][2][3] === '#')
-            }
-            throw Error('error handling outer grid')
-          }
-          return cells.concat(adjChar === '#')
-        }, [] as boolean[])
-
-        // console.log(tempAdjBugCount.length)
-        if (tempAdjBugCount.length !== 4) {
-          if (tempAdjBugCount.length !== 8) {
-            throw Error('too many checks')
-          }
-        }
-        const adjBugCount = tempAdjBugCount.filter(Boolean).length
-
-        if (cell === '#') {
-          const resultChar = (adjBugCount === 1 ? '#' : '.')
-          newRecMap[level][pos.row][pos.col] = resultChar
-          return
-        }
-        if (cell === '.') {
-          const resultChar = (0 < adjBugCount && adjBugCount < 3 ? '#' : '.')
-          newRecMap[level][pos.row][pos.col] = resultChar
-          return
-        }
-        throw Error(`Char '${cell}' cannot be processed`)
-      })
-    })
-  })
-
-  return passTimeRec(newRecMap, maxTime, time + 1)
+function mapHasBugOnPerimeter(map: string[][]) {
+  const top = map[0]
+  const bottom = map[map.length - 1]
+  const left = map.map(row => row[0])
+  const right = map.map(row => row[row.length - 1])
+  return Boolean(
+    top.concat(bottom)
+    .concat(left)
+    .concat(right)
+    .filter(char => char === '#')
+    .length
+  )
 }
 
-function passTimeRecV2(recMap: RecMap, maxTime: number, time = 0) {
-  Object.keys(recMap).toSorted((a, b) => Number(a) - Number(b)).forEach(levelStr => {
-    console.log(levelStr)
-    printMap(recMap[Number(levelStr)])
-  })
-  console.log('END')
-  if (time === maxTime) return recMap
-  const newRecMap = JSON.parse(JSON.stringify(recMap))
-  Object.keys(recMap).forEach(levelStr => {
-    const level = Number(levelStr)
-    recMap[level].forEach((row, i) => {
-      row.forEach((cell, j) => {
-        const pos = { row: i, col: j }
-        if (pos.row === 2 && pos.col === 2) return cell
-        const adjBugCount = directions.reduce((cells, dir) => {
-          const adjPos = translatePos(pos, dir)
-
-          if (adjPos.row === 2 && adjPos.col === 2) {
-            // check inner map
-            // inner map is positive
-            if (!recMap[level + 1]) recMap[level + 1] = createNewLevel()
-            if (!newRecMap[level + 1]) newRecMap[level + 1] = createNewLevel()
-
-            if (pos.row === 1 && pos.col === 2) {
-              return cells.concat(recMap[level + 1][0].map(cell => cell === '#'))
-            }
-            if (pos.row === 3 && pos.col === 2) {
-              return cells.concat(recMap[level + 1][4].map(cell => cell === '#'))
-            }
-            if (pos.row === 2 && pos.col === 1) {
-              const col = recMap[level + 1].map(row => row[0])
-              return cells.concat(col.map(cell => cell === '#'))
-            }
-            if (pos.row === 2 && pos.col === 3) {
-              const col = recMap[level + 1].map(row => row[4])
-              return cells.concat(col.map(cell => cell === '#'))
-            }
-            throw Error('failed to proccess inner map')
-          }
-
-          const adjChar = charAtPos(recMap[level], adjPos)
-          if (!adjChar) {
-            // check outer map
-            // outer map is negative
-            if (!recMap[level - 1]) recMap[level - 1] = createNewLevel()
-            if (!newRecMap[level - 1]) newRecMap[level - 1] = createNewLevel()
-            if (adjPos.row === -1) {
-              return cells.concat(recMap[level - 1][1][2] === '#')
-            }
-            if (adjPos.row === 5) {
-              return cells.concat(recMap[level - 1][3][2] === '#')
-            }
-            if (adjPos.col === -1) {
-              return cells.concat(recMap[level - 1][2][1] === '#')
-            }
-            if (adjPos.col === 5) {
-              return cells.concat(recMap[level - 1][2][3] === '#')
-            }
-            throw Error('failed to process outer map')
-          }
-
-          return cells.concat(charAtPos(map, adjPos) === '#')
-        }, [] as boolean[]).filter(Boolean).length
-
-        if (cell === '#') {
-          newRecMap[level][pos.row][pos.col] = (adjBugCount === 1 ? '#' : '.')
-          return
-        }
-        if (cell === '.') {
-          newRecMap[level][pos.row][pos.col] = (0 < adjBugCount && adjBugCount < 3 ? '#' : '.')
-          return
-        }
-        throw Error(`Char '${cell}' cannot be processed`)
-      })
-    })
-  })
-  return passTimeRecV2(newRecMap, maxTime, time + 1)
-}
-
-function passTimeRecV3(recMap: RecMap, maxTime: number, time: number = 0) {
-  Object.keys(recMap).toSorted((a, b) => Number(a) - Number(b)).forEach(levelStr => {
-    console.log(levelStr)
-    printMap(recMap[Number(levelStr)])
-  })
+function passTimeRec(recMap: RecMap, maxTime: number, time: number = 0) {
   if (time === maxTime) return recMap
   const newMap = JSON.parse(JSON.stringify(recMap))
   const levels = Object.keys(recMap).map(Number)
   const minLevel = Math.min(...levels)
   const maxLevel = Math.max(...levels)
-  if (!recMap[maxLevel + 1]) {
+  if (mapHasBugOnPerimeter(recMap[maxLevel])) {
     recMap[maxLevel + 1] = createNewLevel()
     newMap[maxLevel + 1] = createNewLevel()
   }
-  if (!recMap[minLevel - 1]) {
+  if (mapHasBugOnPerimeter(recMap[minLevel])) {
     recMap[minLevel - 1] = createNewLevel()
     newMap[minLevel - 1] = createNewLevel()
   }
@@ -264,24 +88,20 @@ function passTimeRecV3(recMap: RecMap, maxTime: number, time: number = 0) {
     recMap[level].forEach((row, i) => {
       row.forEach((cell, j) => {
         const pos = { row: i, col: j }
-        if (pos.row === 2 && pos.col === 2) {
-          // skip this check, this 'tile' represents the inner grid
-          return
-        }
+        // skip this check, this 'tile' represents the inner grid
+        if (pos.row === 2 && pos.col === 2) return
 
         // check all adj tiles,
-        // if adj tile is out of bounds, check outer map (level - 1), total of 4 possibilities
-        // if adj tile is { row: 2, col: 2 }, check inner map (level + 1), total of 8 possibilites
+        // if adj tile is out of bounds, check outer map (level - 1)
+        //  - total of 4 possibilities
+        // if adj tile is { row: 2, col: 2 }, check inner map (level + 1)
+        //  - total of 8 possibilites
 
         const adjCells = directions.reduce((cells, dir) => {
           const adjPos = translatePos(pos, dir)
           if (adjPos.row === 2 && adjPos.col === 2) {
             // check inner map
-            if (!recMap[level + 1]) {
-              // throw Error('should have already made new levels')
-              recMap[level + 1] = createNewLevel()
-              newMap[level + 1] = createNewLevel()
-            }
+            if (!recMap[level + 1]) return cells
             if (pos.row === 1) {
               return cells.concat(recMap[level + 1][0])
             } else if (pos.row === 3) {
@@ -293,13 +113,10 @@ function passTimeRecV3(recMap: RecMap, maxTime: number, time: number = 0) {
             }
             throw Error('failed to handle inner map')
           }
+
           const adjChar = charAtPos(recMap[level], adjPos)
           if (!adjChar) {
-            if (!recMap[level - 1]) {
-              // throw Error('should have already made new levels')
-              recMap[level - 1] = createNewLevel()
-              newMap[level - 1] = createNewLevel()
-            }
+            if (!recMap[level - 1]) return cells
             // check outer map
             if (adjPos.row === -1) {
               return cells.concat(recMap[level - 1][1][2])
@@ -318,29 +135,41 @@ function passTimeRecV3(recMap: RecMap, maxTime: number, time: number = 0) {
 
         const bugCount = adjCells.filter(cell => cell === '#').length
         if (cell === '#') {
-          newMap[level][pos.row][pos.col] = (bugCount === 1 ? '#' : '.')
+          const resultChar = (bugCount === 1 ? '#' : '.')
+          newMap[level][pos.row][pos.col] = resultChar
         } else if (cell === '.') {
-          newMap[level][pos.row][pos.col] = (0 < bugCount && bugCount < 3 ? '#' : '.')
+          const resultChar = (0 < bugCount && bugCount < 3 ? '#' : '.')
+          newMap[level][pos.row][pos.col] = resultChar
         } else {
           throw Error('cant handle char')
         }
       })
     })
   })
-  return passTimeRecV3(newMap, maxTime, time + 1)
+  return passTimeRec(newMap, maxTime, time + 1)
 }
 
 function countRecMap(recMap: RecMap) {
-  const allMaps = Object.keys(recMap).map(levelStr => {
-    const level = Number(levelStr)
-    return recMap[level].flat()
-  })
-  return allMaps.flat().filter(char => char === '#').length
+  return Object.keys(recMap).reduce((total, levelStr) => {
+    return total + (
+      recMap[Number(levelStr)]
+      .flat()
+      .filter(char => char === '#')
+      .length
+    )
+  }, 0)
 }
 
 function printMap(map: string[][]) {
   map.forEach(row => console.log(row.join('')))
   console.log()
+}
+
+function printRecMap(recMap: RecMap) {
+  Object.keys(recMap).toSorted((a, b) => Number(a) - Number(b)).forEach(levelStr => {
+    console.log(levelStr)
+    printMap(recMap[Number(levelStr)])
+  })
 }
 
 const map = (
@@ -350,19 +179,10 @@ const map = (
   .map(line => line.split(''))
 )
 
-// const part1 = calcBiodiversity(findRepeatMap(map))
-// console.log(part1, [ 2129920, 18371095 ].includes(part1))
+const part1 = calcBiodiversity(findRepeatMap(map))
+console.log(part1, [ 2129920, 18371095 ].includes(part1))
 
-type RecMap = Record<number, string[][]>
-const recMap: RecMap = { 0: map }
-// this one seems to actually work
-// it generates way too many maps, but we can fix that later
-const resultMap = passTimeRecV3(recMap, 10)
-console.log(countRecMap(resultMap))
-
-// const newRecMap = passTimeRec(recMap, 10)
-// console.log('bug count', countRecMap(newRecMap))
-// const newRecMapV2 = passTimeRecV2(recMap, 10)
-// console.log(countRecMap(newRecMapV2))
-// printMap(newRecMap[0])
-// printMap(newRecMapV2[0])
+const maxTime = process.argv[2].includes('example') ? 10 : 200
+const newRecMap = passTimeRec({ 0: map }, maxTime)
+const part2 = countRecMap(newRecMap)
+console.log(part2, [ 99, 2075 ].includes(part2))
