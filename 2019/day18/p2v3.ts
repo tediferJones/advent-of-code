278855917228074e5fcb133bfd253252537f96fc
ted <theo_drz@live.com>
2025-02-21 23:22:43 -0500

    further optimized day 18, run time is now just over 1 second

--- 2019/day18/p2v3.ts
+++ 2019/day18/p2v3.ts
@@ -1,8 +1,40 @@
 type Position = { row: number, col: number }
-type GridStateV4 = { pos: Position, steps: number, reqKeys: string[] }
+type GridState = { pos: Position, steps: number, reqKeys: string[] }
 type DestKeys = Record<string, { steps: number, reqKeys: string[] }> 
 type Paths = Record<string, DestKeys>
-type PathState = { keys: string[], steps: number }
+type PathState = { pos: Position[], keys: string[], steps: number }
+type PosMap = TwoWayMap<string, Position>
+
+class TwoWayMap<K, V> {
+  keyVal: Map<K, V>
+  valKey: Map<V, K>
+
+  constructor() {
+    this.keyVal = new Map<K, V>()
+    this.valKey = new Map<V, K>()
+  }
+
+  insert(key: K, val: V) {
+    this.keyVal.set(key, val)
+    this.valKey.set(val, key)
+  }
+
+  getVal(key: K) {
+    return this.keyVal.get(key)
+  }
+
+  getKey(val: V) {
+    return this.valKey.get(val)
+  }
+
+  keys() {
+    return [ ...this.keyVal.keys() ]
+  }
+
+  vals() {
+    return [ ...this.valKey.keys() ]
+  }
+}
 
 const directions = [
   { row:  0, col:  1 },
@@ -38,10 +70,11 @@ function getMaxKeyCount(grid: string[][]) {
   }, 0)
 }
 
-function availableKeysV2(
+function availableKeys(
   grid: string[][],
-  queue: GridStateV4[],
-  seen = new Set<string>(queue.map(state => JSON.stringify(state.pos))),
+  queue: GridState[],
+  // seen = new Set<string>(queue.map(state => JSON.stringify(state.pos))),
+  seen = new Set<string>(queue.map(state => `${state.pos.row},${state.pos.col}`)),
   keys: DestKeys = {}
 ) {
   const current = queue.shift()
@@ -49,19 +82,18 @@ function availableKeysV2(
   const pos = current.pos
   const char = grid[pos.row][pos.col]
   if (!char) throw Error('no char')
-  if (charIsKey(char)) {
-    if (current.steps) {
-      keys[char] = {
-        steps: current.steps,
-        reqKeys: current.reqKeys
-      }
+  if (current.steps && charIsKey(char)) {
+    keys[char] = {
+      steps: current.steps,
+      reqKeys: current.reqKeys
     }
   }
   directions.forEach(dir => {
     const newPos = translatePos(pos, dir)
     const newChar = charAtPos(grid, newPos)
     if (newChar === '#') return
-    const newPosStr = JSON.stringify(newPos)
+    // const newPosStr = JSON.stringify(newPos)
+    const newPosStr = `${newPos.row},${newPos.col}`
     if (seen.has(newPosStr)) return
     seen.add(newPosStr)
 
@@ -79,80 +111,36 @@ function availableKeysV2(
       })
     }
   })
-  return availableKeysV2(grid, queue, seen, keys)
+  return availableKeys(grid, queue, seen, keys)
 }
 
 function getKeyPos(grid: string[][]) {
-  return grid.reduce((keyPos, row, i) => {
-    return {
-      ...keyPos,
-      ...row.reduce((positions, cell, j) => {
-        if (cell === '@' || charIsKey(cell)) positions[cell] = { row: i, col: j }
-        return positions
-      }, {} as Record<string, Position>)
-    }
-  }, {} as Record<string, Position>)
-}
-
-// function getKeyPaths(keyPositions: Record<string, Position>) {
-//   return Object.keys(keyPositions).reduce((paths, key) => {
-//     paths[key] = availableKeysV2([{
-//       pos: keyPositions[key],
-//       steps: 0,
-//       reqKeys: []
-//     }])
-//     return paths
-//   }, {} as Paths)
-// }
-
-type KeyPos = Record<string, Position>
-function getKeyPosV2(grid: string[][]) {
-  // return grid.reduce((positions, row, i) => {
-  //   return positions.concat(
-  //     row.reduce((newPositions, cell, j) => {
-  //       if (![ '.', '#' ].includes(cell) && !charIsDoor(cell)) {
-  //         return newPositions.concat({ row: i, col: j })
-  //       }
-  //       return newPositions
-  //     }, [] as Position[])
-  //   )
-  // }, [] as Position[])
-  return grid.reduce((keyPos, row, i) => {
-    return {
-      ...keyPos,
-      ...row.reduce((positions, cell, j) => {
-        if (![ '.', '#' ].includes(cell) && !charIsDoor(cell)) {
-          positions[cell] = { row: i, col: j }
-        }
-        return positions
-      }, {} as KeyPos)
-    }
-  }, {} as KeyPos)
+  return grid.reduce((twoWayMap, row, i) => {
+    row.forEach((cell, j) => {
+      if (![ '.', '#' ].includes(cell) && !charIsDoor(cell)) {
+        twoWayMap.insert(cell, { row: i, col: j })
+      }
+    })
+    return twoWayMap
+  }, new TwoWayMap() as PosMap)
 }
 
 function replaceStart(grid: string[][]) {
-  let num = 0
-  return grid.map(row => {
-    return row.map(cell => {
-      return cell === '@' ? (num++).toString() : cell
-      // if (cell === '@') return num++
-    })
-  })
+  let count = 0
+  return {
+    newGrid: grid.map(row => {
+      return row.map(cell => {
+        return cell === '@' ? (count++).toString() : cell
+      })
+    }),
+    count,
+  }
 }
 
-function getKeyPathsV2(grid: string[][], keyPositions: KeyPos) {
-// function getKeyPathsV2(positions: Position[]) {
-  // return positions.reduce((paths, pos) => {
-  //   paths[JSON.stringify(pos)] = availableKeysV2([{
-  //     pos,
-  //     steps: 0,
-  //     reqKeys: [],
-  //   }])
-  //   return paths
-  // }, {} as Paths)
-  return Object.keys(keyPositions).reduce((paths, key) => {
-    paths[key] = availableKeysV2(grid, [{
-      pos: keyPositions[key],
+function getKeyPaths(grid: string[][], keyPositions: PosMap) {
+  return keyPositions.keys().reduce((paths, key) => {
+    paths[key] = availableKeys(grid, [{
+      pos: keyPositions.getVal(key)!,
       steps: 0,
       reqKeys: []
     }])
@@ -160,64 +148,54 @@ function getKeyPathsV2(grid: string[][], keyPositions: KeyPos) {
   }, {} as Paths)
 }
 
-function pathsBitMask(state: PathState) {
-  const posChar = state.keys[state.keys.length - 1] || '@'
-  return `${posChar},${state.keys.toSorted()}`
-}
-
-function pathsBitMaskV2(state: PathStateV2) {
+function pathsBitMask(state: PathState, keyCount: number, positions: PosMap) {
   // const posChar = state.keys[state.keys.length - 1] || '@'
   // return `${JSON.stringify(state.pos)},${state.keys.toSorted()}`
 
-  const posStr = state.pos.map(pos => `${pos.row},${pos.col}`).join(':')
-  return `${posStr},${state.keys.toSorted()}`
+  // const posStr = state.pos.map(pos => `${pos.row},${pos.col}`).join(':')
+  // return `${posStr},${state.keys.toSorted()}`
+  // const posStr = state.pos.map(pos => positions.getKey(pos)!).join(':')
+  // const keyMask = Array(keyCount).fill(0)
+  // state.keys.forEach(key => {
+  //   keyMask[key.charCodeAt(0) - 97] = 1
+  // })
+  // return `${posStr}:${keyMask.join('')}`
+
+  const mask = Array(keyCount).fill(0)
+  state.keys.forEach(key => {
+    mask[key.charCodeAt(0) - 97] = 1
+  })
+  state.pos.forEach(pos => {
+    const char = positions.getKey(pos)!
+    mask[char.charCodeAt(0) - 97] = 2
+  })
+  mask.push(state.pos.length)
+  return mask.join('')
 }
 
 function getValidNextPaths(nextPaths: DestKeys, currentKeys: string[]) {
-  return Object.keys(nextPaths)
-    .filter(newKey => !currentKeys.includes(newKey))
-    .filter(newKey => 
-      nextPaths[newKey].reqKeys.every(reqKey => currentKeys.includes(reqKey))
-    )
+  return Object.keys(nextPaths).filter(newKey => {
+    if (currentKeys.includes(newKey)) return
+    return nextPaths[newKey].reqKeys.every(reqKey => currentKeys.includes(reqKey))
+  })
+    // .filter(newKey => !currentKeys.includes(newKey))
+    // .filter(newKey => 
+    //   nextPaths[newKey].reqKeys.every(reqKey => currentKeys.includes(reqKey))
+    // )
 }
 
 const dfsPathsCache: Record<string, number> = {}
 let dfsPathsBest = Infinity
-function dfsPaths(state: PathState, paths: Paths, keyCount: number): number {
-  const cacheKey = pathsBitMask(state)
-  if (state.keys.length === keyCount) {
-    // console.log('found', state.steps)
-    if (state.steps < dfsPathsBest) dfsPathsBest = state.steps
-    return state.steps
-  }
-  if (dfsPathsCache[cacheKey]) {
-    const shortCutResult = state.steps + dfsPathsCache[cacheKey]
-    if (shortCutResult < dfsPathsBest) dfsPathsBest = shortCutResult
-    return shortCutResult
-  }
-  const posChar = state.keys[state.keys.length - 1] || '@'
-  const nextPaths = paths[posChar]
-  const validPaths = getValidNextPaths(paths[posChar], state.keys)
-  const result = validPaths.reduce((best, newKey) => {
-    const newState = {
-      steps: state.steps + nextPaths[newKey].steps,
-      keys: state.keys.concat(newKey)
-    }
-    if (newState.steps > dfsPathsBest) return best
-    return Math.min(dfsPaths(newState, paths, keyCount), best)
-  }, Infinity)
-  dfsPathsCache[cacheKey] = result - state.steps
-  return result
-}
-
-type PathStateV2 = { pos: Position[], keys: string[], steps: number }
-function dfsPathsV2(
-  state: PathStateV2,
+function dfsPaths(
+  state: PathState,
   paths: Paths,
   keyCount: number,
-  positions: KeyPos
+  positions: PosMap,
 ): number {
-  const cacheKey = pathsBitMaskV2(state)
+  // const startCacheKeyTime = Bun.nanoseconds()
+  const cacheKey = pathsBitMask(state, keyCount, positions)
+  // cacheKeyTime += Bun.nanoseconds() - startCacheKeyTime
+  // console.log(cacheKey)
   if (state.keys.length === keyCount) {
     if (state.steps < dfsPathsBest) dfsPathsBest = state.steps
     return state.steps
@@ -227,39 +205,28 @@ function dfsPathsV2(
     if (shortCutResult < dfsPathsBest) dfsPathsBest = shortCutResult
     return shortCutResult
   }
-  // const posChar = state.keys[state.keys.length - 1] || '@'
-  // const nextPaths = paths[posChar]
 
   // eliminate this nested reduce
   // reduce state.pos into all possible next states,
   // then reduce nextStates to least steps
+  // pos is required to know create new pos array
   const result = state.pos.reduce((best, pos) => {
-    const key = Object.keys(positions).find(key => positions[key] === pos)
-    if (!key) {
-      console.log(pos, key, state.pos)
-      throw Error('cant find key')
-    }
-    // const nextPaths = paths[JSON.stringify(pos)]
+    const key = positions.getKey(pos)!
     const nextPaths = paths[key]
-    if (!nextPaths) {
-      console.log(key, paths)
-    }
-    // console.log(key, paths[key])
-    // console.log(positions)
-    // console.log(nextPaths, paths)
     const validPaths = getValidNextPaths(nextPaths, state.keys)
+    const newPositions = state.pos.filter(oldPos => oldPos !== pos)
     const result = validPaths.reduce((best, newKey) => {
       const newState = {
-        pos: state.pos.filter(newPos => newPos !== pos).concat(positions[newKey]),
+        pos: newPositions.concat(positions.getVal(newKey)!),
         steps: state.steps + nextPaths[newKey].steps,
         keys: state.keys.concat(newKey)
       }
-      // console.log(newState.pos.length)
       if (newState.steps > dfsPathsBest) return best
-      return Math.min(dfsPathsV2(newState, paths, keyCount, positions), best)
+      return Math.min(dfsPaths(newState, paths, keyCount, positions), best)
     }, Infinity)
     return Math.min(result, best)
   }, Infinity)
+
   dfsPathsCache[cacheKey] = result - state.steps
   return result
 }
@@ -298,6 +265,20 @@ function splitGrid(grid: string[][]) {
   return newGrid
 }
 
+function solveGrid(grid: string[][]) {
+  const { newGrid, count } = replaceStart(grid)
+  const keyCount = getMaxKeyCount(newGrid)
+  const positions = getKeyPos(newGrid)
+  const paths = getKeyPaths(newGrid, positions)
+  const pos = Array(count).fill(0).map((_, i) => positions.getVal(i.toString())!)
+  return dfsPaths(
+    { keys: [], steps: 0, pos },
+    paths,
+    keyCount,
+    positions,
+  )
+}
+
 function printGrid(grid: string[][]) {
   grid.forEach(row => console.log(row.join('')))
 }
@@ -310,84 +291,10 @@ const grid = (
   .map(row => row.split(''))
 )
 
-const keyCount = getMaxKeyCount(grid)
-
-// const part1 = dfsPaths({ keys: [], steps: 0 }, getKeyPaths(getKeyPos(grid)), keyCount)
-// console.log(part1, [ 8, 86, 132, 136, 81, 3866 ].includes(part1))
-// console.log(`TIME: ${(Bun.nanoseconds() - startTime) / 10**9} seconds`)
-
-const newGrid = replaceStart(grid)
-const positions = getKeyPosV2(newGrid)
-const paths = getKeyPathsV2(newGrid, positions)
-// console.log(positions)
-// console.log(paths)
-const part1 = dfsPathsV2(
-  { keys: [], steps: 0, pos: [ positions['0'] ] },
-  paths,
-  keyCount,
-  positions,
-)
+const part1 = solveGrid(grid)
 console.log(part1, [ 8, 86, 132, 136, 81, 3866 ].includes(part1))
 
-const gridPart2 = replaceStart(splitGrid(grid))
-// printGrid(gridPart2)
-const part2Positions = getKeyPosV2(gridPart2)
-const part2Paths = getKeyPathsV2(gridPart2, part2Positions)
-// console.log(part2Paths)
-// printGrid(gridPart2)
-// console.log([ 0, 1, 2, 3 ].map(num => part2Positions[num.toString()]))
-const part2 = dfsPathsV2(
-  { keys: [], steps: 0, pos: [ 0, 1, 2, 3 ].map(num => part2Positions[num.toString()]) },
-  part2Paths,
-  keyCount,
-  part2Positions,
-)
+const part2 = solveGrid(splitGrid(grid))
 console.log(part2, [ 8, 24, 32, 72, 1842 ].includes(part2))
+
 console.log(`TIME: ${(Bun.nanoseconds() - startTime) / 10**9} seconds`)
-
-// const paths = Object.keys(keyPositions).reduce((paths, key) => {
-//   paths[key] = availableKeysV2([{
-//     pos: keyPositions[key],
-//     steps: 0,
-//     reqKeys: []
-//   }])
-//   return paths
-// }, {} as Paths)
-// console.log(paths)
-
-// const graphSet = new Set<string>()
-// const graphCache: Record<string, number> = {}
-// let best = Infinity;
-// function bfsPaths(queue: PathState[]) {
-//   const current = queue.shift()
-//   if (!current) return
-//   if (current.steps > best) return bfsPaths(queue)
-//   console.log(current.steps, queue.length)
-//   if (current.keys.length === keyCount) {
-//     best = current.steps
-//     return bfsPaths(queue)
-//   }
-//   const posChar = current.keys[current.keys.length - 1] || '@'
-//   const cacheKey = `${posChar},${current.keys.toSorted()}`
-//   // if (graphSet.has(cacheKey)) return bfsPaths(queue)
-//   // graphSet.add(cacheKey)
-//   if (current.steps >= graphCache[cacheKey]) return bfsPaths(queue)
-//   graphCache[cacheKey] = current.steps
-//   const availableKeys = Object.keys(paths[posChar]).filter(key => {
-//     return paths[posChar][key].reqKeys.every(reqKey => current.keys.includes(reqKey)) && !current.keys.includes(key)
-//   })
-// 
-//   const newPaths = availableKeys.map(key => ({
-//     steps: paths[posChar][key].steps + current.steps,
-//     keys: current.keys.concat(key)
-//   }))
-// 
-//   return bfsPaths(queue.concat(newPaths))
-//   return bfsPaths(queue.concat(newPaths).toSorted((a, b) => {
-//     const stepDiff = a.steps - b.steps
-//     const keyDiff = b.keys.length - a.keys.length
-//     if (keyDiff) return keyDiff
-//     return stepDiff
-//   }))
-// }
-// console.log(bfsPaths([{ keys: [], steps: 0 }]))
