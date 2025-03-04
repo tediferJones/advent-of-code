c080f4cd38cfa74412e8ec6a83f230dfb068813a
ted <theo_drz@live.com>
2025-03-03 23:36:31 -0500

    added logic to flip equation and converted all numbers to bigints, might have a working solution for part 2

--- 2019/day22/p2v5.ts
+++ 2019/day22/p2v5.ts
@@ -41,9 +41,7 @@ function buildEqV2(eq: EqMod, shuffles: string[]) {
   const shuffleType = Object.keys(shuffleTypesV2).find(key => shuffle.includes(key)) as Shuffles
   if (!shuffleType) throw Error(`cannot find shuffle type for: ${shuffle}`)
   const [ num ] = shuffle.match(/-?\d+/)?.map(Number) || [ Infinity ]
-  // console.log(shuffleType, num)
   const newEq = composeEq(shuffleTypesV2[shuffleType](num), eq)
-  // console.log(newEq)
   return buildEqV2(newEq, shuffles.slice(1))
 }
 
@@ -66,51 +64,70 @@ function repeatShuffle(eq: EqMod, count: number) {
   return g
 }
 
-// const shuffleTypesV3: Record<Shuffles, (eq: EqMod, n: number) => EqMod> = {
-//   'new stack': (eq) => {
-//     // compose eq with y = -x - 1
-//     // y = -(eq.m * x + eq.b) - 1
-//     // y = -eq.m * x - eq.b - 1
-//     return {
-//       m: eq.m * -1,
-//       b: (eq.b * -1) - 1,
-//       mod: eq.mod
-//     }
-//   },
-//   'cut': (eq, n) => {
-//     // compose eq with y = x - n
-//     // y = (eq.m * x + eq.b) - n
-//     // y = eq.m * x + eq.b - n
-//     return {
-//       m: eq.m,
-//       b: eq.b - n,
-//       mod: eq.mod
-//     }
-//   },
-//   'increment': (eq, n) => {
-//     // compose eq with y = n * x
-//     // y = n * (eq.m * x + eq.b)
-//     // y = n * eq.m * x + n * eq.b
-//     console.log('increment', n, eq.m * n)
-//     return {
-//       m: eq.m * n,
-//       b: eq.b * n,
-//       mod: eq.mod
-//     }
-//   }
-// }
-// 
-// function buildEqV3(eq: EqMod, shuffles: string[]) {
-//   const shuffle = shuffles[0]
-//   if (!shuffle) return eq
-//   const shuffleType = Object.keys(shuffleTypesV2).find(key => shuffle.includes(key)) as Shuffles
-//   if (!shuffleType) throw Error(`cannot find shuffle type for: ${shuffle}`)
-//   const [ num ] = shuffle.match(/-?\d+/)?.map(Number) || [ Infinity ]
-//   console.log(shuffleType, num)
-//   const newEq = shuffleTypesV3[shuffleType](eq, num)
-//   console.log(newEq)
-//   return buildEqV3(newEq, shuffles.slice(1))
-// }
+function getFinalPos(eq: EqMod, startPos: number) {
+  return (eq.m * startPos + eq.b) % eq.mod
+}
+
+function getFinalCard(eq: EqMod, cardNum: number) {
+  // invert equation (might need to use modInv)
+  // y = (mx + b) % mod
+  // y - b = mx
+  // (y - b) / m = x
+  // x = (y - b) / m
+  //
+  // OR
+  // (3x === 4) mod 7
+  // return modInv(cardNum - eq.b)
+  // return ((cardNum - eq.b) / eq.m) % eq.mod
+  const inv = modInv(eq.m, eq.mod)
+  return mod(((cardNum - eq.b) * inv), eq.mod)
+}
+
+function solvePart1(pos: number, deckSize: number, shuffles: string[]) {
+  const initEq = { m: 1, b: 0, mod: deckSize }
+  const shuffledEq = buildEqV2(initEq, shuffles)
+  return getFinalPos(shuffledEq, pos)
+}
+
+function solvePart2(
+  pos: number,
+  deckSize: number,
+  shuffleCount: number,
+  shuffles: string[]
+) {
+  const initEq = { m: 1, b: 0, mod: deckSize }
+  const shuffleEq = buildEqV2(initEq, shuffles)
+  const multiShuffleEq = repeatShuffle(shuffleEq, shuffleCount)
+  return getFinalCard(multiShuffleEq, pos)
+}
+
+const shuffleOrder = (
+  (await Bun.file(process.argv[2]).text())
+  .split(/\n/)
+  .filter(Boolean)
+)
+
+// TESTING
+// it is important to note that the resulting equation will output the final position of the input
+// example:
+// eq(2019) => 5136; meaning card 2019 will move to position 5136
+//
+// eq(2019) absolutely does not indicate which card will end up at position 2019
+// const initEq = { m: 1, b: 0, mod: 10007 }
+// const builtEq = buildEqV2(initEq, shuffleOrder)
+// console.log('builtEq', builtEq)
+// console.log('part1', getFinalPos(builtEq, 2019))
+// console.log('part1 backward', getFinalCard(builtEq, 5169))
+
+const part1 = solvePart1(2019, 10007, shuffleOrder)
+console.log(part1, [ 5169 ].includes(part1))
+
+const part2 = solvePart2(2020, 119315717514047, 101741582076661, shuffleOrder)
+console.log(part2)
+
+// Test repeated shuffles
+// console.log(repeatShuffle(builtEq, 2))
+// console.log(composeEq(builtEq, builtEq))
 
 function modInv(a: number, m: number): number {
   let m0 = m;
@@ -139,81 +156,70 @@ function modInv(a: number, m: number): number {
   return x;
 }
 
-// type EqState = { x: number, deckSize: number }
-type ShuffleFunc = (i: number, deckSize: number, num: number) => number
-const shuffleTypes: Record<string, ShuffleFunc> = {
-  // 'new stack': (eq: EqState) => eq.deckSize - eq.x - 1,
-  // 'cut': (eq: EqState, num: number) => (eq.x - num) % eq.deckSize,
-  // 'increment': (eq: EqState, num: number) => (eq.x * num) % eq.deckSize
-  'new stack': (i) => (i * -1) - 1,
-  'cut': (i, deckSize, num) => (i - num) % deckSize,
-  'increment': (i, deckSize, num) => (i * num) % deckSize
-}
+// ##############################################
+// ######## CAN ONLY TRACK INDEX FORWARD ########
+// ##############################################
+// // type EqState = { x: number, deckSize: number }
+// type ShuffleFunc = (i: number, deckSize: number, num: number) => number
+// const shuffleTypes: Record<string, ShuffleFunc> = {
+//   // 'new stack': (eq: EqState) => eq.deckSize - eq.x - 1,
+//   // 'cut': (eq: EqState, num: number) => (eq.x - num) % eq.deckSize,
+//   // 'increment': (eq: EqState, num: number) => (eq.x * num) % eq.deckSize
+//   'new stack': (i) => (i * -1) - 1,
+//   'cut': (i, deckSize, num) => (i - num) % deckSize,
+//   'increment': (i, deckSize, num) => (i * num) % deckSize
+// }
+// 
+// function handleShuffles(
+//   // eq: EqState,
+//   index: number,
+//   deckSize: number,
+//   shuffles: string[],
+// ) {
+//   const shuffle = shuffles[0]
+//   if (!shuffle) return index
+//   // console.log('shuffle:', shuffle)
+//   const shuffleType = Object.keys(shuffleTypes).find(key => shuffle.includes(key))
+//   if (!shuffleType) throw Error(`cant find shuffle type: ${shuffle}`)
+//   // console.log('shuffle type:', shuffleType)
+//   const [ num ] = shuffle.match(/-?\d+/)?.map(Number) || [ Infinity ]
+//   // const newIndex = shuffleTypes[shuffleType]({ x: index, deckSize }, num)
+//   const newIndex = shuffleTypes[shuffleType](index, deckSize, num)
+//   // return handleShuffles(newIndex, deckSize, shuffles.slice(1))
+//   return handleShuffles(fixNegative(newIndex, deckSize), deckSize, shuffles.slice(1))
+// }
+// 
+// function fixNegative(num: number, deckSize: number) {
+//   if (num >= 0) return num
+//   return fixNegative(num + deckSize, deckSize)
+// }
+// 
+// function showDeck(deckSize: number, shuffleOrder: string[]) {
+//   const temp: number[] = Array(deckSize).fill(0)
+//   return temp.reduce((arr, _, i) => {
+//     const newIndex = handleShuffles(i, deckSize, shuffleOrder)
+//     // console.log(`${i} -> ${newIndex}`)
+//     return arr.with(newIndex, i)
+//   }, temp)
+// }
+// 
+// const shuffleOrder = (
+//   (await Bun.file(process.argv[2]).text())
+//   .split(/\n/)
+//   .filter(Boolean)
+// )
+// 
+// // console.log(showDeck(10, shuffleOrder))
+// // console.log(handleShuffles(0, 10, shuffleOrder))
+// 
+// // part1 test
+// // WORKING
+// // console.log(handleShuffles(2019, 10007, shuffleOrder))
+// // console.log(showDeck(10007, shuffleOrder)[5169])
 
-function handleShuffles(
-  // eq: EqState,
-  index: number,
-  deckSize: number,
-  shuffles: string[],
-) {
-  const shuffle = shuffles[0]
-  if (!shuffle) return index
-  // console.log('shuffle:', shuffle)
-  const shuffleType = Object.keys(shuffleTypes).find(key => shuffle.includes(key))
-  if (!shuffleType) throw Error(`cant find shuffle type: ${shuffle}`)
-  // console.log('shuffle type:', shuffleType)
-  const [ num ] = shuffle.match(/-?\d+/)?.map(Number) || [ Infinity ]
-  // const newIndex = shuffleTypes[shuffleType]({ x: index, deckSize }, num)
-  const newIndex = shuffleTypes[shuffleType](index, deckSize, num)
-  // return handleShuffles(newIndex, deckSize, shuffles.slice(1))
-  return handleShuffles(fixNegative(newIndex, deckSize), deckSize, shuffles.slice(1))
-}
-
-function fixNegative(num: number, deckSize: number) {
-  if (num >= 0) return num
-  return fixNegative(num + deckSize, deckSize)
-}
-
-function showDeck(deckSize: number, shuffleOrder: string[]) {
-  const temp: number[] = Array(deckSize).fill(0)
-  return temp.reduce((arr, _, i) => {
-    const newIndex = handleShuffles(i, deckSize, shuffleOrder)
-    // console.log(`${i} -> ${newIndex}`)
-    return arr.with(newIndex, i)
-  }, temp)
-}
-
-const shuffleOrder = (
-  (await Bun.file(process.argv[2]).text())
-  .split(/\n/)
-  .filter(Boolean)
-)
-
-// TESTING
-// it is important to note that the resulting equation will output the final position of the input
-// example:
-// eq(2019) => 5136; meaning card 2019 will move to position 5136
-//
-// eq(2019) absolutely does not indicate which card will end up at position 2019
-const initEq = { m: 1, b: 0, mod: 10007 }
-const builtEq = buildEqV2(initEq, shuffleOrder)
-console.log('builtEq', builtEq)
-// console.log(builtEq)
-// console.log((builtEq.m * 2019 + builtEq.b) % builtEq.mod)
-// console.log(showDeckV2(builtEq))
-// console.log('result', repeatShuffle(builtEq, 2))
-// console.log('check', buildEqV2(builtEq, shuffleOrder))
-console.log(repeatShuffle(builtEq, 2))
-console.log(composeEq(builtEq, builtEq))
-
-// console.log(showDeck(10, shuffleOrder))
-// console.log(handleShuffles(0, 10, shuffleOrder))
-
-// part1 test
-// WORKING
-// console.log(handleShuffles(2019, 10007, shuffleOrder))
-// console.log(showDeck(10007, shuffleOrder)[5169])
-
+// ######################################
+// ######## PROBABLY DOESNT WORK ########
+// ######################################
 // function buildEq(eq: Eq, shuffles: string[]) {
 //   const current = shuffles[0]
 //   if (!current) return eq
